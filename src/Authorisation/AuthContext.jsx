3610d5b9c2683f874550ef58ef9ec7731d9e13f3
rdvisiontech <hr@rdvision.tech>
2024-11-06 15:35:39 +0530

    Update AuthContext.jsx

--- src/Authorisation/AuthContext.jsx
+++ src/Authorisation/AuthContext.jsx
@@ -4,8 +4,8 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isDisabled, setIsDisabled] = useState(false);
-  // const url ="https://backend.rdvision.tech"
-  const url ="http://localhost:8080"
+  const url ="https://backend.rdvision.tech"
+  // const url ="http://localhost:8080"
   const token =localStorage.getItem("loginDetails")?JSON.parse(localStorage.getItem("loginDetails")).token:""
   return (
     <AuthContext.Provider value={{ isDisabled, setIsDisabled,url,token}}>
